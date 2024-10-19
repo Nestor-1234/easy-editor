b4fdc3e2d659dea7f5fb662de6bf97384f254d34
Nestor-1234 <ovcarenkonestor206@gmail.com>
2024-10-19 13:33:05 +0300

    1

---
+++ main.py
@@ -0,0 +1,73 @@
+from PyQt5.QtWidgets import *
+import os
+
+app = QApplication([])
+
+window = QWidget()
+window.setWindowTitle("Easy Editor")
+window.resize(700, 400)
+
+
+list_files = QListWidget()
+lbl_image = QLabel("Картинка")
+btn_dir = QPushButton("Папка")
+btn_left = QPushButton("Вліво")
+btn_right = QPushButton("Правло")
+btn_flip = QPushButton("Відзеркалити")
+btn_sharp = QPushButton("Різкість")
+btn_Black_white = QPushButton("Ч/Б")
+
+row = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+
+col1.addWidget(btn_dir)
+col1.addWidget(list_files)
+
+col2.addWidget(lbl_image, 95)
+
+row_tools = QHBoxLayout()
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_Black_white)
+col2.addLayout(row_tools)
+
+row.addLayout(col1, 20)
+row.addLayout(col2, 80)
+
+window.setLayout(row)
+
+window.show()
+
+workdir = ""
+
+def choosseWorkDir():
+    global workdir 
+    workdir = QFileDialog.getExistingDirectory()
+
+
+def filter(files, extensions):
+    result = []
+    for filename in files:
+        for ext in extensions:
+            if filename.endswith(ext):
+                result.append(filename)
+    return result
+
+def showFilenamesList():
+    extensions = [".jpg",".png",".jpeg",".gif",".bmp"]
+    choosseWorkDir()
+    filenames = filter(os.listdir(workdir), extensions)
+    list_files.clear()
+    for filename in filenames :
+        list_files.addItem(filename)
+
+
+
+
+
+btn_dir.clicked.connect(showFilenamesList)
+app.exec_()
+
